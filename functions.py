def9405beafb51766a047ce56da9f7c4f7263389
Arisber73 <atemnikow@yandex.ru>
2022-10-17 15:08:11 +0400

    добавление функции write_ini

--- functions.py
+++ functions.py
@@ -1,9 +1,7 @@
 """Дополнительный модуль: вспомогательные функции."""
-
 # ИСПОЛЬЗОВАТЬ: из стандартной библиотеки лучше импортировать только необходимые объекты, чем модули целиком — последни могут быть весьма объёмными
 from configparser import ConfigParser
 from pprint import pprint
-
 from data import STATS, players_file
 
 
@@ -14,16 +12,25 @@ def read_ini(file: str, statistics: dict) -> None:
     for section in config.sections():
         statistics[section] = {key: int(value) for key, value in config[section].items()}
 
+def write_ini(file: str, statistics: dict) -> None:
+    """Записывает данные из глобальной переменной STATS в ini - файл."""
+    config = ConfigParser()
+    config.read_dict(statistics)
+    with open(file, 'w') as players_stats:
+        config.write(players_stats)
 
-# ДОБАВИТЬ: функцию write_ini()
-
-
-# ИСПОЛЬЗОВАТЬ: не надо лишних сущностей — убираете тесты под проверку импорта и выполняете текущий модуль, этого достаточно
 if __name__ == '__main__':
+    read_ini(players_file, STATS)
+    pprint(STATS)
+    STATS['Arisber'] = {'wins': 5, 'losses': 4, 'ties': 3}
+    write_ini(players_file, STATS)
     read_ini(players_file, STATS)
     pprint(STATS)
 
 
 # std_out:
-# {'Player1': {'loses': 3, 'ties': 1, 'wins': 2},
-#  'Player2': {'lose': 0, 'tie': 0, 'wins': 0}}
+# {'Player1': {'losses': 3, 'ties': 1, 'wins': 2},
+#  'Player2': {'losses': 0, 'ties': 0, 'wins': 0}}
+# {'Arisber': {'losses': 4, 'ties': 3, 'wins': 5},
+#  'Player1': {'losses': 3, 'ties': 1, 'wins': 2},
+#  'Player2': {'losses': 0, 'ties': 0, 'wins': 0}}
