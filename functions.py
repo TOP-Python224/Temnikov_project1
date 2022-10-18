f08a17ffd395987a3d75ca909f9990e6ae9ea801
Arisber73 <atemnikow@yandex.ru>
2022-10-18 06:23:14 +0400

    Добавление функции get_nickname()

--- functions.py
+++ functions.py
@@ -3,8 +3,14 @@
 from configparser import ConfigParser
 from pprint import pprint
 
-from data import STATS, players_file, RANGE, BOARD, DIM
+from data import STATS, SAVES, players_file, saves_file, RANGE, BOARD, DIM, PLAYERS
 
+def get_nickname() -> list:
+    player_1 = input('Игрок_1 - введите свой никнейм: ')
+    player_2 = input('Игрок_2 - введите свой никнейм: ')
+    PLAYERS.clear()
+    PLAYERS.insert(0, player_1)
+    PLAYERS.insert(1, player_2)
 
 def read_ini(file: str, statistics: dict) -> None:
     """Считывает статистику об игроках из ini-файла и записывает данные в глобальную переменную STATS."""
@@ -12,7 +18,6 @@ def read_ini(file: str, statistics: dict) -> None:
     config.read(file, encoding='utf-8')
     for section in config.sections():
         statistics[section] = {key: int(value) for key, value in config[section].items()}
-
 
 def write_ini(file: str, statistics: dict) -> None:
     """Записывает данные из глобальной переменной STATS в ini - файл."""
